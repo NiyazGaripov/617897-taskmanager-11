e57eace06327204dbaa79c9d07bbc5bb9652e71b
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-03-31 22:34:25 +0300

    Creates function generate markup button

--- src/main.js
+++ src/main.js
@@ -360,3 +360,9 @@ const createTaskEditCardComponent = () => {
     </article>`
   );
 };
+
+const createLoadMoreButtonComponent = () => {
+  return (
+    `<button class="load-more" type="button">load more</button>`
+  );
+};
