78de104f16a691683351de91350e5fdd5de23129
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-04-10 13:50:45 +0300

    Creates func set time format

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,3 +1,7 @@
 export const renderComponent = (container, component, place = `beforeend`) => {
   container.insertAdjacentHTML(place, component);
 };
+
+const setTimeFormat = (value) => {
+  return value < 10 ? `0${value}` : String(value);
+};
