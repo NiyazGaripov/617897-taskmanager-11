2fd25a7c2d22f31a9db218eb7f3ac8f35d54c67d
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-04-11 20:13:29 +0300

    Imports module filters and param to createFilterComponent

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import {createTaskEditCardComponent} from './components/task-edit-card.js';
 import {createTaskCardComponent} from './components/task-card.js';
 import {createLoadMoreButtonComponent} from './components/load-more-button.js';
 import {generateCards} from './mock/card.js';
+import {generateFilters} from './mock/filter.js';
 
 const TASK_CARDS_AMOUNT = 22;
 const TASK_CARDS_AMOUNT_ON_START = 8;
@@ -13,9 +14,10 @@ const TASK_CARDS_AMOUNT_LOAD_MORE = 8;
 const pageMainElement = document.querySelector(`.main`);
 const pageMenuElement = pageMainElement.querySelector(`.main__control`);
 const cards = generateCards(TASK_CARDS_AMOUNT);
+const filters = generateFilters();
 
 renderComponent(pageMenuElement, createNavigationMenuComponent());
-renderComponent(pageMainElement, createFilterComponent());
+renderComponent(pageMainElement, createFilterComponent(filters));
 renderComponent(pageMainElement, createSortComponent());
 
 const taskCardsElement = pageMainElement.querySelector(`.board__tasks`);
