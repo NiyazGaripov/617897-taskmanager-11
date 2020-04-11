71fe4ba1c9c5ca0078abfd495edc073af4d071d9
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-04-11 20:06:14 +0300

    Creates filtersComponent

--- src/components/filter.js
+++ src/components/filter.js
@@ -15,7 +15,9 @@ const createFiltersComponent = (filters, isChecked) => {
   );
 };
 
-const createFilterComponent = () => {
+const createFilterComponent = (filters) => {
+  const filtersComponent = filters.map((filter, index) => createFiltersComponent(filter, index === 0)).join(`\n`);
+
   return (
     `<section class="main__filter filter container">
       <input
