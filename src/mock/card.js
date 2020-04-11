823f6c01e74b335349def92ee24702041a78d9fc
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-04-11 17:41:59 +0300

    Creates func get random date

--- src/mock/card.js
+++ src/mock/card.js
@@ -1,3 +1,5 @@
+import {getRandomIntegerNumber, getRandomArrayItem} from './../utils.js';
+
 const DESCRIPTION_CARDS = [
   `Изучить теорию`,
   `Сделать домашку`,
@@ -12,6 +14,16 @@ const DefaultRepeatingDays = {
   "fr": false,
   "sa": false,
   "su": false,
+};
+
+const getRandomDate = () => {
+  const targetDate = new Date();
+  const sign = Math.random() > 0.5 ? 1 : -1;
+  const diffValue = sign * getRandomIntegerNumber(0, 8);
+
+  targetDate.setDate(targetDate.getDate() + diffValue);
+
+  return targetDate;
 };
 
 const generateCard = () => {
