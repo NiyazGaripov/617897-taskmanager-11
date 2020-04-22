2eccdc527d32058117f9b42f52e155c69f1a6a44
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-04-22 21:24:00 +0300

    Removes unused code and renders nav menu and filters

--- src/main.js
+++ src/main.js
@@ -19,37 +19,5 @@ const pageMenuElement = pageMainElement.querySelector(`.main__control`);
 const cards = generateCards(TASK_CARDS_AMOUNT);
 const filters = generateFilters();
 
-// renderComponent(pageMenuElement, createNavigationMenuComponent());
-// renderComponent(pageMainElement, createFilterComponent(filters));
-// renderComponent(pageMainElement, createSortComponent());
-
-// const taskCardsElement = pageMainElement.querySelector(`.board__tasks`);
-// const boardElement = pageMainElement.querySelector(`.board`);
-
-// renderComponent(taskCardsElement, createTaskEditCardComponent(cards[0]));
-
-// let showingTaskCards = TASK_CARDS_AMOUNT_ON_START;
-
-// cards
-//   .slice(BEGIN_INDEX, showingTaskCards)
-//   .forEach((card) => {
-//     renderComponent(taskCardsElement, createTaskCardComponent(card));
-//   });
-
-// renderComponent(boardElement, createLoadMoreButtonComponent());
-
-// const loadMoreButton = boardElement.querySelector(`.load-more`);
-
-// loadMoreButton.addEventListener(`click`, () => {
-//   const prevTaskCards = showingTaskCards;
-//   showingTaskCards = showingTaskCards + TASK_CARDS_AMOUNT_LOAD_MORE;
-
-//   cards
-//     .slice(prevTaskCards, showingTaskCards)
-//     .forEach((card) => {
-//       renderComponent(taskCardsElement, createTaskCardComponent(card));
-//     });
-//   if (showingTaskCards >= cards.length) {
-//     loadMoreButton.remove();
-//   }
-// });
+renderComponent(pageMenuElement, new NavigationMenu().getElement(), RenderPosition.BEFOREEND);
+renderComponent(pageMainElement, new Filter(filters).getElement(), RenderPosition.BEFOREEND);
