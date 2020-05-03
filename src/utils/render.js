0ccc4f577552df3475e8d2c272af8ec4b7aaf40d
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-05-03 22:05:11 +0300

    Creates module render and transfers renders functions

---
+++ src/utils/render.js
@@ -0,0 +1,24 @@
+const RenderPosition = {
+  AFTERBEGIN: `afterbegin`,
+  BEFOREEND: `beforeend`
+};
+
+const renderComponent = (container, component, place = `beforeend`) => {
+  switch (place) {
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(component);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(component);
+      break;
+  }
+};
+
+const createElement = (template) => {
+  const newElement = document.createElement(`div`);
+  newElement.innerHTML = template;
+
+  return newElement.firstChild;
+};
+
+export {RenderPosition, renderComponent, createElement};
