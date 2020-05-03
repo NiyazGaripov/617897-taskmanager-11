c2c73d7625b158d2e32254d55b2f037512e2d9c9
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-05-03 22:06:11 +0300

    Creates module math and transfers math functions

---
+++ src/utils/math.js
@@ -0,0 +1,11 @@
+const getRandomIntegerNumber = (min, max) => {
+  return min + Math.floor(Math.random() * (max - min));
+};
+
+const getRandomArrayItem = (array) => {
+  const randomItem = getRandomIntegerNumber(0, array.length);
+
+  return array[randomItem];
+};
+
+export {getRandomIntegerNumber, getRandomArrayItem};
