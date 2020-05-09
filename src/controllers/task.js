923cda1e6325a134a6f5884f2ead3512b30af846
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-05-09 18:20:10 +0300

    Imports dependences

---
+++ src/controllers/task.js
@@ -0,0 +1,3 @@
+import {renderComponent, replaceComponent} from './../utils/render.js';
+import {TaskCard} from './../components/task-card.js';
+import {TaskEditCard} from './../components/task-edit-card.js';
