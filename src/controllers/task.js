dbad7fdd1364bfcfd1149405db1ff091b89755df
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-05-09 18:20:32 +0300

    Creates class TaskController

--- src/controllers/task.js
+++ src/controllers/task.js
@@ -1,3 +1,13 @@
 import {renderComponent, replaceComponent} from './../utils/render.js';
 import {TaskCard} from './../components/task-card.js';
 import {TaskEditCard} from './../components/task-edit-card.js';
+
+class TaskController {
+  constructor(container) {
+  }
+
+  render(task) {
+  }
+}
+
+export {TaskController};
