6622d3d9361f8d886b405a86b97b2349d0e332ae
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-05-11 18:24:24 +0300

    Creates mode, method setDefaultView and adds onViewChange

--- src/controllers/task.js
+++ src/controllers/task.js
@@ -3,16 +3,26 @@ import {TaskCard} from './../components/task-card.js';
 import {TaskEditCard} from './../components/task-edit-card.js';
 import {ESC_KEYCODE} from './../constants.js';
 
+const Mode = {
+  DEFAULT: `default`,
+  EDIT: `edit`,
+};
+
 class TaskController {
-  constructor(container, onDataChange) {
+  constructor(container, onDataChange, onViewChange) {
     this._container = container;
     this._onDataChange = onDataChange;
+    this._onViewChange = onViewChange;
+    this._mode = Mode.DEFAULT;
     this._taskComponent = null;
     this._taskEditComponent = null;
     this._onEscKeyDown = this._onEscKeyDown.bind(this);
   }
 
   render(card) {
+    const oldTaskComponent = this._taskComponent;
+    const oldTaskEditComponent = this._taskEditComponent;
+
     this._taskComponent = new TaskCard(card);
     this._taskEditComponent = new TaskEditCard(card);
 
@@ -38,16 +48,31 @@ class TaskController {
       this._replaceEditToTask();
     });
 
-    renderComponent(this._container, this._taskComponent);
+    if (oldTaskEditComponent && oldTaskComponent) {
+      replaceComponent(this._taskComponent, oldTaskComponent);
+      replaceComponent(this._taskEditComponent, oldTaskEditComponent);
+    } else {
+      renderComponent(this._container, this._taskComponent);
+    }
+  }
+
+  setDefaultView() {
+    if (this._mode !== Mode.DEFAULT) {
+      this._replaceEditToTask();
+    }
   }
 
   _replaceTaskToEdit() {
+    this._onViewChange();
     replaceComponent(this._taskEditComponent, this._taskComponent);
+    this._mode = Mode.EDIT;
   }
 
   _replaceEditToTask() {
     document.removeEventListener(`keydown`, this._onEscKeyDown);
+    this._taskEditComponent.reset();
     replaceComponent(this._taskComponent, this._taskEditComponent);
+    this._mode = Mode.DEFAULT;
   }
 
   _onEscKeyDown(evt) {
