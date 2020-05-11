7b88cceff172048c3f325fe0c6dce72fe1155ce3
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-05-11 18:45:59 +0300

    Adds getDate and removes month names

--- src/components/task-edit-card.js
+++ src/components/task-edit-card.js
@@ -1,5 +1,5 @@
-import {MONTH_NAMES, DAYS, COLORS} from './../constants.js';
-import {getTime} from './../utils/time.js';
+import {DAYS, COLORS} from './../constants.js';
+import {getDate, getTime} from './../utils/time.js';
 import {createRepeatingDaysComponent} from './days.js';
 import {createColorsComponent} from './colors.js';
 import {AbstractSmartComponent} from './abstract-smart-component.js';
